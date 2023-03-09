ae30cc2375dd8332c06d5e6ee6cf5ed825011029
mazzaro <49665452+mazzaro1@users.noreply.github.com>
2023-03-09 10:33:30 -0300

    projeto finalizado
    projeto finalizado.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 
-function tocaSom(idElementAudio){
-    document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+
+   if (elemento != null && elemento.localName ==='audio'){
+    elemento.play();
+   }
+   else {
+    alert('esse elemento não existe')
+   }
 }
 
 
@@ -8,19 +15,26 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSom;
 
-let i = 0;
 
-while ( i < listaDeTeclas.length){
+
+
+for (let i = 0; i < listaDeTeclas.length ; i++){
 
     const tecla = listaDeTeclas[i];
-
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`
 
     tecla.onclick = function(){
         tocaSom(idAudio)
     };
 
-    i++;
+    tecla.onkeydown = function (evento){
+        if (evento.code ==='Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+            tecla.classList.remove('ativa');
+    }
 }
+
